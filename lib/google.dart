9d6917307fef4d9fa6887773d9230a40c779793d
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-16 20:36:39 -0400

    DUH! Parse URI!

--- lib/google.dart
+++ lib/google.dart
@@ -3,7 +3,7 @@ part of directbot;
 void google(CommandEvent event) {
     if (event.args.length >= 1) {
         String query = Uri.encodeComponent(event.args.join(" "));
-        new HttpClient().getUrl('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' + query).then((HttpClientRequest req) {
+        new HttpClient().getUrl(Uri.parse('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' + query)).then((HttpClientRequest req) {
             return req.close();
         }).then((HttpClientResponse response) {
             return response.transform(UTF8.decoder);
