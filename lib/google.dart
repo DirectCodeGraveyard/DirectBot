b179630378ea9a66fd2d1e1b4b3a6dbe3edf776d
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-16 20:39:37 -0400

    Wut

--- lib/google.dart
+++ lib/google.dart
@@ -3,14 +3,17 @@ part of directbot;
 void google(CommandEvent event) {
     if (event.args.length >= 1) {
         String query = Uri.encodeComponent(event.args.join(" "));
+        var results;
         new HttpClient().getUrl(Uri.parse('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' + query)).then((HttpClientRequest req) {
             return req.close();
         }).then((HttpClientResponse response) {
             return response.transform(UTF8.decoder);
         }).then((content) {
-            var resp = JSON.decode(content);
-            var results = resp["responseData"]["results"];
-            if  (results.length == 0) {
+            var resp = JSON.decoder.convert(content);
+            results = resp["responseData"]["results"];
+            return results.length;
+        }).then((length) {
+            if  (length == 0) {
                 event.reply("> No Results Found!");
             } else {
                 var result = results[0];
