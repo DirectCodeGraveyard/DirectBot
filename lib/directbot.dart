25f1232643f2dcf850ed20ff7f417eb5013c38dd
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-16 21:15:48 -0400

    Fun

--- lib/directbot.dart
+++ lib/directbot.dart
@@ -136,6 +136,22 @@ start() {
           event.reply("> Much Fun Wow: http://i.imgur.com/TLb1ZQz.gif");
       });
 
+      bot.command("who").listen((CommandEvent event) {
+          var msg = event.args.join(" ");
+          switch(msg) {
+              case "is epic":
+              case "is the best programmer":
+                event.reply("> samrg472");
+                break;
+              case "wants the d":
+                event.reply("> she wants the d.");
+                break;
+              default:
+                event.reply("> LOL IDK!");
+                break;
+          }
+      });
+
       bot.command("part").listen((event) {
           if (!check_user(event)) return;
           if (event.args.length != 1) {
