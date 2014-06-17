25f420859289935c1506000ba669b34852b74510
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-16 20:41:11 -0400

    YAF

--- lib/google.dart
+++ lib/google.dart
@@ -3,22 +3,19 @@ part of directbot;
 void google(CommandEvent event) {
     if (event.args.length >= 1) {
         String query = Uri.encodeComponent(event.args.join(" "));
-        var results;
         new HttpClient().getUrl(Uri.parse('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' + query)).then((HttpClientRequest req) {
             return req.close();
         }).then((HttpClientResponse response) {
-            return response.transform(UTF8.decoder);
-        }).then((content) {
-            var resp = JSON.decoder.convert(content);
-            results = resp["responseData"]["results"];
-            return results.length;
-        }).then((length) {
-            if  (length == 0) {
-                event.reply("> No Results Found!");
-            } else {
-                var result = results[0];
-                event.reply("> ${result["titleNoFormatting"]} | ${result["url"]}");
-            }
+            response.transform(UTF8.decoder).join("").then((content) {
+                var resp = JSON.decoder.convert(content);
+                var results = resp["responseData"]["results"];
+                if  (results.length == 0) {
+                    event.reply("> No Results Found!");
+                } else {
+                    var result = results[0];
+                    event.reply("> ${result["titleNoFormatting"]} | ${result["url"]}");
+                }
+            });
         });
     } else {
         event.reply("> Usage: google <query>");
