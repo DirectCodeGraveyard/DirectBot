768f43e40eb7ef1d6c867830e99e32220f674d0b
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-17 21:12:31 -0400

    Smarter YouTube Information Handling

--- lib/youtube.dart
+++ lib/youtube.dart
@@ -1,26 +1,26 @@
 part of directbot;
 
-var _yt_link_regex = new RegExp(r'\(?\b((http|https)://|www[.])[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]');
+var link_regex = new RegExp(r'\(?\b((http|https)://|www[.])[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]');
 
 var _yt_info_url = 'https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&key=${googleAPIKey}&id=';
 
 var _yt_link_id = new RegExp(r'^.*(youtu.be/|v/|embed/|watch\?|youtube.com/user/[^#]*#([^/]*?/)*)\??v?=?([^#\&\?]*).*');
 
 handle_youtube(event) {
-  if (_yt_link_regex.hasMatch(event.message)) {
-    _yt_link_regex.allMatches(event.message).forEach((match) {
+  if (link_regex.hasMatch(event.message)) {
+    link_regex.allMatches(event.message).forEach((match) {
       var url = match.group(0);
-      if (!url.contains("youtube"))
-        return;
-      var request_url = "${_yt_info_url}${extract_yt_id(url)}";
-      http.getUrl(Uri.parse(request_url)).then((request) => request.close()).then((response) {
-        response.transform(UTF8.decoder).join("").then((content) {
-          var data = JSON.decoder.convert(content);
-          var items = data['items'];
-          var video = items[0];
-          print_yt_info(event, video);
+      if (url.contains("youtube") || url.contains("youtu.be")) {
+        var request_url = "${_yt_info_url}${extract_yt_id(url)}";
+        http.getUrl(Uri.parse(request_url)).then((request) => request.close()).then((response) {
+          response.transform(UTF8.decoder).join("").then((content) {
+            var data = JSON.decoder.convert(content);
+            var items = data['items'];
+            var video = items[0];
+            print_yt_info(event, video);
+          });
         });
-      });
+      }
     });
   }
 }
