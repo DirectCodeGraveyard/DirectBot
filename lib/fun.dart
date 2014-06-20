f79d71ba24eb56e8eac39e3709e1fa239d904e34
TheMike <zeusintraining@gmail.com>
2014-06-20 14:17:11 -0400

    Works now. Let the high-fives fly.

--- lib/fun.dart
+++ lib/fun.dart
@@ -3,9 +3,12 @@ part of directbot;
 String current_highfiver = null;
 
 handle_highfive(event) {
-  if(current_highfiver == null) {
-    current_highfiver = event.client.getUsername();
-  } else {
-    event.reply("> " + current_highfiver + " high fived " + event.client.getUsername() + "! http://www.ihighfive.com/");
+  if((event.message.indexOf("\u005Co") != -1 || event.message.indexOf("o/") != -1) && event.client.getNickname() != event.from && event.message.indexOf('\u005o/') == -1) {
+    if(current_highfiver == null) {
+      current_highfiver = event.from;
+    } else {
+      event.reply("> " + current_highfiver + " high fived " + event.from + "!");
+      current_highfiver = null;
+    }
   }
 }
