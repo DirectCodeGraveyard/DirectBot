828f7b284f77dd9c9646e2c84e93cd17d95502d8
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-23 14:45:54 -0400

    Add support for regex /g modifier

--- lib/regex.dart
+++ lib/regex.dart
@@ -4,8 +4,13 @@ class regex {
   static void handle(MessageEvent event) {
     if (event.message.startsWith("s/") && event.message.length > 3) {
       var msg = event.message.substring(2); // skip "s/"
+      var first = false;
       if (msg.endsWith("/"))
         msg = msg.substring(0, msg.length - 1);
+      else if (msg.endsWith("/g")) {
+        msg = msg.substring(0, msg.length - 2);
+        first = true;
+      }
       
       var index = msg.indexOf("/");
       var expr = msg.substring(0, index);
@@ -16,7 +21,9 @@ class regex {
       var events = Buffer.get(event.channel.name);
       for (event in events) {
         if (regex.hasMatch(event.message)) {
-          var e = new MessageEvent(event.client, event.from, event.target, event.message.replaceAll(regex, replacement));
+          var dat_msg = event.message;
+          var newmsg = first ? dat_msg.replaceFirst(regex, replacement) : dat_msg.replaceAll(regex, replacement);
+          var e = new MessageEvent(event.client, event.from, event.target, newmsg);
           event.reply(event.from + ": " + e.message);
           Buffer.handle(e);
           return;
