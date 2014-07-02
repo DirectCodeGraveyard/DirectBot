e639cfbbf28440c99a3895a099c95b90b06393fe
Kenneth Endfinger <kaendfinger@gmail.com>
2014-07-01 20:52:22 -0400

    Sunday

--- lib/src/fun.dart
+++ lib/src/fun.dart
@@ -2,9 +2,15 @@ part of directbot;
 
 void register_fun_commands() {
   bot.command("who").listen((CommandEvent event) {
-    var msg = event.args.join(" ");
+    var msg = event.args.join(" ").trim();
+    if (msg.endsWith("?")) {
+      msg = msg.substring(0, msg.length - 1);
+    }
     switch (msg) {
       case "is epic":
+      case "is the best coder":
+      case "writes the best code":
+      case "makes the best code":
       case "is the best programmer":
         event.reply("> samrg472");
         break;
@@ -13,6 +19,9 @@ void register_fun_commands() {
         break;
       case "cares":
         event.reply("> http://www.youtube.com/watch?v=RFZrzg62Zj0");
+        break;
+      case "likes KitKats":
+        event.reply("> kaendfinger");
         break;
       default:
         var url = "http://lmgtfy.com/?q=${Uri.encodeComponent("who " + msg)}";
