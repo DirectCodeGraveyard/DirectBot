2c057bed3a5e2be193115bb43301c727b93e1200
samrg472 <samrg472@gmail.com>
2014-07-08 13:06:47 -0400

    Remove parts of the who command

--- lib/src/fun.dart
+++ lib/src/fun.dart
@@ -7,18 +7,6 @@ void register_fun_commands() {
       msg = msg.substring(0, msg.length - 1);
     }
     switch (msg) {
-      case "is epic":
-      case "is the best coder":
-      case "writes the best code":
-      case "makes the best code":
-      case "is the best programmer":
-        event.reply("> samrg472");
-        break;
-      case "is the worst code":
-      case "is the worst coder":
-      case "is the worse programmer":
-        event.reply("> Clank");
-        break;
       case "wants the d":
         event.reply("> she wants the d.");
         break;
