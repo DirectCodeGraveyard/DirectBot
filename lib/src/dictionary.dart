b5b2bfa251fce58cc4a229e02a1c70681fc6d2a8
Kenneth Endfinger <kaendfinger@gmail.com>
2014-07-13 01:36:47 -0400

    Add Define Command using Wordnik

--- lib/src/dictionary.dart
+++ lib/src/dictionary.dart
@@ -16,4 +16,20 @@ void register_dictionary_commands() {
       }
     });
   });
+  
+  bot.command("define").listen((CommandEvent event) {
+    if (event.args.length == 0) {
+      event.reply("> Usage: define <word>");
+      return;
+    }
+    var word = event.args.join(" ");
+    http.get("http://api.wordnik.com/v4/word.json/${Uri.encodeComponent(word)}/definitions?limit=50&includeRelated=true&useCanonical=true&includeTags=false&api_key=50b6a4b7956e5934de00d0e43c10b78e7a67da3eef3e50662").then((http.Response resp) {
+      var data = JSON.decode(resp.body);
+      if (data.length == 0) {
+        event.reply("> No Definition Found");
+      } else {
+        event.reply("> ${data.first["word"]}: ${data.first["text"]}");
+      }
+    });
+  });
 }
