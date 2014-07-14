5c39c84682fb13fb85f4ee20f7be6f8d75f6534c
Kenneth Endfinger <kaendfinger@gmail.com>
2014-07-13 21:12:14 -0400

    Epic Sticky Channels

--- lib/src/sticky_channels.dart
+++ lib/src/sticky_channels.dart
@@ -6,7 +6,7 @@ List<String> sticky_channels;
 
 void setup_sticky_channels() {
   sticky_channels = config.list("sticky_channels");
-  sticky_timer = new Timer(new Duration(seconds: 1), () {
+  sticky_timer = new Timer.periodic(new Duration(seconds: 1), (timer) {
     sticky_channels.forEach((chan) {
       if (bot.client.channel(chan) == null) {
         bot.join(chan);
