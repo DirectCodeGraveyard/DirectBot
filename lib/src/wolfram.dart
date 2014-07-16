4560c6859543968b79002e95977eacf041bacb65
Kenneth Endfinger <kaendfinger@gmail.com>
2014-07-16 15:32:39 -0400

    Arrange Wolfram API Stuff

--- lib/src/wolfram.dart
+++ lib/src/wolfram.dart
@@ -8,38 +8,38 @@ class Wolfram {
       return new Future.value(JSON.decode(resp.body));
     });
   }
-}
-
-String meaningful_output(Map<String, dynamic> input) {
-  var result = input["queryresult"];
   
-  try {
-    if (result["pods"] == null) {
-      if (result["didyoumeans"] != null && result["didyoumeans"].length > 0) {
-        var word = result["didyoumeans"][0]["val"];
-        return "Did you mean '${word}'?";
-      } else {
-        return "ERROR: No Result Found"; 
+  static String meaningful_output(Map<String, dynamic> input) {
+    var result = input["queryresult"];
+    
+    try {
+      if (result["pods"] == null) {
+        if (result["didyoumeans"] != null && result["didyoumeans"].length > 0) {
+          var word = result["didyoumeans"][0]["val"];
+          return "Did you mean '${word}'?";
+        } else {
+          return "ERROR: No Result Found"; 
+        }
       }
+      
+      var pods = {};
+      
+      result["pods"].forEach((pod) {
+        pods[pod["title"]] = pod;
+      });
+      
+      if (pods.containsKey("Definition")) {
+        return pods["Definition"]["subpods"][0]["plaintext"];
+      } else if (pods.containsKey("Result")) {      
+        return pods["Result"]["subpods"][0]["plaintext"];
+      }
+    } catch (e) {
+      print(e.stackTrace);
+      return "ERROR: Failed to Get Output";
     }
     
-    var pods = {};
-    
-    result["pods"].forEach((pod) {
-      pods[pod["title"]] = pod;
-    });
-    
-    if (pods.containsKey("Definition")) {
-      return pods["Definition"]["subpods"][0]["plaintext"];
-    } else if (pods.containsKey("Result")) {      
-      return pods["Result"]["subpods"][0]["plaintext"];
-    }
-  } catch (e) {
-    print(e.stackTrace);
-    return "ERROR: Failed to Get Output";
+    return "ERROR: Not Yet Supported";
   }
-  
-  return "ERROR: Not Yet Supported";
 }
 
 void register_wolfram_commands() {
@@ -49,7 +49,7 @@ void register_wolfram_commands() {
     } else {
       Achievements.give(event.from, "Wolfram|Alpha User");
       Wolfram.get(event.args.join(" ")).then((it) {
-        event.reply("> " + meaningful_output(it));
+        event.reply("> " + Wolfram.meaningful_output(it));
       });
     }
   });
